2827d9fb64b0416ebf5438af1808421b8e5317c7
fan <907551651@qq.com>
2016-05-25 18:09:42 +0800

    基本的手势动作完成

--- app/src/main/java/com/app/fan/pathview/PathView.java
+++ app/src/main/java/com/app/fan/pathview/PathView.java
@@ -1,33 +1,96 @@
 package com.app.fan.pathview;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
-import android.widget.RelativeLayout;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by fan on 2016/5/24.
  */
-public class PathView extends RelativeLayout {
-    private Paint mPaint;
+public class PathView extends View {
+    private Paint mPaint = new Paint();
+    private Path mPath = new Path();
+    private float pathX;
+    private float pathY;
+    private Boolean isActionUp = false;
 
     public PathView(Context context) {
-        super(context, null);
-    }
-
-    public PathView(Context context, AttributeSet attrs) {
-        super(context, attrs, 0);
-    }
-
-    public PathView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        super(context);
         init(context);
     }
 
+    public PathView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+
     private void init(Context context) {
-        mPaint = new Paint();
+        Log.d("path", "init");
+        mPaint.setColor(ContextCompat.getColor(context, R.color.colorPrimaryDark));
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeWidth(1f);
+        mPaint.setStrokeWidth(5.0f);
 
     }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (isActionUp) {
+            isActionUp = false;
+            mPaint.setStyle(Paint.Style.STROKE);
+            return true;
+        }
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchDown(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(event);
+                break;
+            case MotionEvent.ACTION_UP:
+                mPath.close();
+                mPaint.setStyle(Paint.Style.FILL);
+
+
+                break;
+
+        }
+        postInvalidate();
+        return true;
+    }
+
+    private void touchMove(MotionEvent event) {
+        float disX = Math.abs(event.getX() - pathX);
+        float disy = Math.abs(event.getY() - pathY);
+        if (disX > 5 || disy > 5) {
+            mPath.lineTo(event.getX(), event.getY());
+            pathX = event.getX();
+            pathY = event.getY();
+            Log.d("path", "touchMove" + pathX + ":" + pathY);
+
+        }
+    }
+
+    private void touchDown(MotionEvent event) {
+        mPath.reset();
+        pathX = event.getX();
+        pathY = event.getY();
+        mPath.moveTo(pathX, pathY);
+        isActionUp=true;
+        Log.d("path", "touchDown" + pathX + ":" + pathY);
+    }
+
+
 }
